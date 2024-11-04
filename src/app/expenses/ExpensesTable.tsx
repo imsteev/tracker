50e2a5ca541940fc1271e871ace01b868ad4bc0b
Stephen Chung <spchung95@gmail.com>
2024-11-03 21:39:38 -0500

    table styling

--- src/app/expenses/ExpensesTable.tsx
+++ src/app/expenses/ExpensesTable.tsx
@@ -7,25 +7,29 @@ import {
   TableCell,
   Table,
 } from "@/components/ui/table";
+import { expenses } from "@/db/schema";
+import { db } from "@/db/setup";
+import { desc } from "drizzle-orm";
 
-export const ExpensesTable = ({ expenses }: { expenses: any[] }) => {
+export const ExpensesTable = async () => {
+  const es = await db.select().from(expenses).orderBy(desc(expenses.date));
   return (
     <Table>
       <TableCaption>A list of your recent expenses.</TableCaption>
       <TableHeader>
         <TableRow>
           <TableHead>ID</TableHead>
-          <TableHead>Date</TableHead>
+          <TableHead className="text-nowrap">Date</TableHead>
           <TableHead>Description</TableHead>
           <TableHead className="text-right">Amount</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {expenses.map((e) => {
+        {es.map((e) => {
           return (
             <TableRow key={e.id}>
               <TableCell className="font-medium">{e.id}</TableCell>
-              <TableCell>{e.date}</TableCell>
+              <TableCell className="text-nowrap">{e.date}</TableCell>
               <TableCell>{e.description}</TableCell>
               <TableCell className="text-right">{e.amount}</TableCell>
             </TableRow>
--- src/app/expenses/page.tsx
+++ src/app/expenses/page.tsx
@@ -1,22 +1,12 @@
-import { db } from "@/db/setup";
-import { desc } from "drizzle-orm";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { expenses } from "@/db/schema";
 import { ExpensesTable } from "./ExpensesTable";
+import { Suspense } from "react";
 
 export default async function Expenses() {
-  const es = await db.select().from(expenses).orderBy(desc(expenses.date));
   return (
     <div className="w-full h-screen flex justify-center">
-      <ExpensesTable expenses={es} />
+      <Suspense fallback={<p className="mt-4">Loading...</p>}>
+        <ExpensesTable />
+      </Suspense>
     </div>
   );
 }
